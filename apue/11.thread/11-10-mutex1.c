480a088041f244991cb7ae3292603a518f7ac951
xychen <cxybjfu@126.com>
2021-08-20 21:54:18 +0800

    线程同步1

---
+++ apue/11.thread/11-10-mutex1.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <pthread.h>
+
+struct foo
+{
+    int f_count;
+    pthread_mutex_t f_lock;
+    int f_id;
+};
+
+struct foo *foo_alloc(int id)
+{
+    struct foo *fp;
+
+    if ((fp = malloc(sizeof(struct foo))) != NULL)
+    {
+        fp->f_count = 1;
+        fp->f_id = id;
+        //初始化锁
+        if (pthread_mutex_init(&fp->f_lock, NULL) != 0)
+        {
+            free(fp);
+            return (NULL);
+        }
+    }
+
+    return (fp);
+}
+
+void foo_hold(struct foo *fp)
+{
+    pthread_mutex_lock(&fp->f_lock);
+    fp->f_count++;
+    pthread_mutex_unlock(&fp->f_lock);
+}
+
+void foo_rele(struct foo *fp)
+{
+    pthread_mutex_lock(&fp->f_lock);
+    if (--fp->f_count == 0)
+    {
+        //释放锁
+        pthread_mutex_unlock(&fp->f_lock);
+        //销毁锁
+        pthread_mutex_destroy(&fp->f_lock);
+        free(fp);
+    }
+    else
+    {
+        pthread_mutex_unlock(&fp->f_lock);
+    }
+}
