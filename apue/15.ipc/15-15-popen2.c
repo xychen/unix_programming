9fc1fe1a55a0249c352fdf18797683fad22acf52
xychen <cxybjfu@126.com>
2021-08-27 22:12:20 +0800

    popen2

---
+++ apue/15.ipc/15-15-popen2.c
@@ -0,0 +1,33 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(void)
+{
+    char line[MAXLINE];
+    FILE *fpin;
+
+    // if ((fpin = popen("/home/cxy/mygithub/unix_programming/apue/15.ipc/bin/15-14-uclc", "r")) == NULL)
+    if ((fpin = popen("/home/cxy/mygithub/unix_programming/apue/15.ipc/bin/15-14-uclc", "r")) == NULL)
+    {
+        err_sys("popen error");
+    }
+    for (;;)
+    {
+        fputs("prompt> ", stdout);
+        fflush(stdout);
+        if (fgets(line, MAXLINE, fpin) == NULL)
+        {
+            break;
+        }
+        if (fputs(line, stdout) == EOF)
+        {
+            err_sys("fputs error to pipe");
+        }
+    }
+    if (pclose(fpin) == -1)
+    {
+        err_sys("pclose error");
+    }
+    putchar('\n');
+    exit(0);
+}
